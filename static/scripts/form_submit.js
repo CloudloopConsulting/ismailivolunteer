33f8f8911a9f0a719013be6a6350fd4c03809064
Alizain Feerasta <alizain.feerasta@gmail.com>
2017-03-16 16:59:25 -0400

    Updated static builds

--- static/scripts/form_submit.js
+++ static/scripts/form_submit.js
@@ -12619,48 +12619,47 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 
   return parsley;
 });
-//# sourceMappingURL=parsley.js.map
+
 });
 
 window.jQuery = jquery;
 
-
-const $ = window.jQuery;
-const $form = $('form[data-form-marker]').eq(0);
-const form = $form[0];
-let parsedConfig;
+var $ = window.jQuery;
+var $form = $('form[data-form-marker]').eq(0);
+var form = $form[0];
+var parsedConfig = void 0;
 
 function formAction(config) {
-	return 'https://formspree.io/' + config.to
+	return 'https://formspree.io/' + config.to;
 }
 
 function formMethod() {
-	return 'POST'
+	return 'POST';
 }
 
 function formCC(config) {
-	return $('<input type="hidden" name="_cc" value="' + (config.cc || []).join(',') + '">')[0]
+	return $('<input type="hidden" name="_cc" value="' + (config.cc || []).join(',') + '">')[0];
 }
 
 try {
 	parsedConfig = JSON.parse(atob(window.__FORM_DATA__));
 } catch (e) {
-	throw new Error('could not decode formdata')
+	throw new Error('could not decode formdata');
 }
 
 if (Object.keys(parsedConfig) === 0) {
-	throw new Error('incomplete config')
+	throw new Error('incomplete config');
 }
 
 if (!form) {
-	throw new Error('cannot find form')
+	throw new Error('cannot find form');
 }
 
-const instance = $form.parsley({
+var instance = $form.parsley({
 	errorClass: 'has-danger',
 	successClass: 'has-success',
 	classHandler: function classHandler(el) {
-		return el.$element.closest('.form-group')
+		return el.$element.closest('.form-group');
 	},
 	errorsWrapper: '<div class="feedback-container"></div>',
 	errorTemplate: '<div class="form-control-feedback"></div>'
@@ -12671,19 +12670,17 @@ console.log(parsedConfig);
 form.onsubmit = function onsubmit(ev) {
 	if (!instance.isValid()) {
 		ev.preventDefault();
-		return false
+		return false;
 	}
 	form.appendChild(formCC(parsedConfig));
 	form.action = formAction(parsedConfig);
 	form.method = formMethod();
 	window._form = form;
 	ev.preventDefault();
-	return false
+	return false;
 };
 
-var form_submit = {
-
-};
+var form_submit = {};
 
 return form_submit;
 
