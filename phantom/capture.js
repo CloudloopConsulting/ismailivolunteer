cbe08d08787495c2dd51046ffb2bf458a69b9ce4
Alizain Feerasta <alizain.feerasta@gmail.com>
2017-04-15 13:17:38 -0400

    Major improvements

---
+++ phantom/capture.js
@@ -0,0 +1,40 @@
+/* eslint-env phantomjs */
+
+const webpage = require('webpage')
+const system = require('system')
+
+if (system.args.length < 2 || system.args.length > 3) {
+	console.log('Usage: phantom.js URL filename')
+	phantom.exit(1)
+} else {
+	console.log('Capturing page')
+
+	const page = webpage.create()
+	const url = system.args[1]
+	const filename = system.args[2]
+
+	page.viewportSize = {
+		width: 1200,
+		height: 630
+	}
+
+	page.settings.userAgent = 'Mozilla/5.0 (Macintosh Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.57 Safari/537.17'
+
+	page.customHeaders = {
+		Referer: url
+	}
+
+	page.open(url, function (status) {
+		if (status !== 'success') {
+			console.log('Unable to open URL')
+			phantom.exit(1)
+		} else {
+			console.log('Opened URL')
+			window.setTimeout(function () {
+				console.log('Saving image')
+				page.render(filename)
+				phantom.exit(0)
+			}, 2000)
+		}
+	})
+}
---
+++ phantom/runner.js
@@ -0,0 +1,42 @@
+const Promise = require('bluebird')
+const fs = Promise.promisifyAll(require('fs'))
+const path = require('path')
+const exec = require('child_process').exec
+
+const HTTP_ROOT = 'http://localhost:1313/phantom'
+const ROOT = path.join(__dirname, '..')
+const SRC_DIR = path.join(ROOT, 'content', 'phantom')
+const IMG_DIR = path.join(ROOT, 'images', 'social')
+const IMG_EXT = 'png'
+
+// path.parse().name
+
+function getURL(name) {
+	return path.join(HTTP_ROOT, name)
+}
+
+function getImageFile(name) {
+	return `${path.join(IMG_DIR, name)}.${IMG_EXT}`
+}
+
+async function saveFile(file) {
+	const name = path.parse(file).name
+	return new Promise((resolve, reject) => {
+		exec(`./node_modules/.bin/phantomjs phantom/capture.js ${getURL(name)} ${getImageFile(name)}`, { cwd: ROOT }, (err, stdout, stderr) => {
+			if (err) {
+				console.log(stdout)
+				console.log(stderr)
+				reject()
+			}
+			resolve()
+		})
+	})
+}
+
+async function getFiles(dir) {
+	return fs.readdirAsync(dir)
+}
+
+getFiles(SRC_DIR)
+	.then(data => Promise.all(data.map(saveFile)))
+	.then(() => console.log('DONE'))
